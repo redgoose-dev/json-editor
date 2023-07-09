767ae41377eb556dab98f8fed8352c7803c398cf
redgoose.eth <scripter@me.com>
2023-07-09 15:57:17 +0900

    일부 정리

--- src/dev/App.svelte
+++ src/dev/App.svelte
@@ -1,19 +1,9 @@
 <header>dev-app-header</header>
-<div id="foo"></div>
 <!--<json-editor/>-->
 <JsonEditor foo="111222"/>
 <!--<json-editor foo="roo3425oooo"/>-->
 <footer>dev-app-footer</footer>
 
 <script>
-import { onMount } from 'svelte'
 import JsonEditor from '../json-editor/index.svelte'
-import JsonEditor2 from './json-editor-lib.js'
-
-onMount(() => {
-  const foo = document.getElementById('foo')
-  customElements.define('json-editor-2', JsonEditor2.element)
-  foo.innerHTML = `<json-editor-2 foo="123123333"/>`
-
-})
 </script>
